a40afe2d4356a5fb8bebba68b5a236a7af7e7ae4
Richard Whitehill <whiteric@jlab.org>
2024-09-18 16:03:24 -0400

    update

--- HW2/.ipynb_checkpoints/prob4-checkpoint.py
+++ HW2/.ipynb_checkpoints/prob4-checkpoint.py
@@ -4,7 +4,7 @@ import numpy as np
 N = int(1e6)
 x = np.random.uniform(size=N)
 y = 2*np.random.uniform(size=N)
-res = np.mean(np.sin(x**2+y**2))
+res = 2*1*np.mean(np.sin(x**2+y**2))
 print(res)
 
 # 4.2
@@ -12,7 +12,7 @@ N = int(1e6)
 r  = np.random.uniform(size=N)
 th = 2*np.pi*np.random.uniform(size=N)
 x,y = np.sqrt(r)*np.cos(th),np.sqrt(r)*np.sin(th)
-res = np.mean(np.exp(-(x**2+y**2)))
+res = np.pi*1**2*np.mean(np.exp(-(x**2+y**2)))
 print(res)
 
 # 4.3
@@ -20,8 +20,7 @@ N = int(1e6)
 x = np.random.uniform(size=N)
 y = np.random.uniform(size=N)
 cond = y < 1-x
-x,y = x[cond],y[cond]
-res = np.mean(x+y)
+res = 1*1*np.mean((x+y)*cond)
 print(res)
 
 # 4.4
@@ -30,7 +29,7 @@ x1 = np.random.uniform(size=N)
 x2 = np.random.uniform(size=N)
 x3 = np.random.uniform(size=N)
 x4 = np.random.uniform(size=N)
-res = np.mean(np.exp(-(x1**2 + x2**2 + x3**2 + x4**2)))
+res = 1*1*1*1*np.mean(np.exp(-(x1**2 + x2**2 + x3**2 + x4**2)))
 print(res)
 
 # 4.5
@@ -39,7 +38,6 @@ x1 = np.random.uniform(size=N)
 x2 = np.random.uniform(size=N)
 x3 = np.random.uniform(size=N)
 x4 = np.random.uniform(size=N)
-cond = x1**2 + x2**2 + x3**2 + x4**2 < 1
-res = np.mean((x1**2 + x2**2 + x3**2 + x4**2)*np.exp(-(x1**2 + x2**2 + x3**2 + x4**2)))
+cond = (x1**2 + x2**2 + x3**2 + x4**2) < 1
+res = 2*2*2*2*np.mean(cond*(x1**2 + x2**2 + x3**2 + x4**2)*np.exp(-(x1**2 + x2**2 + x3**2 + x4**2)))
 print(res)
-
